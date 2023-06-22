c2518e18a6bbe140f0338290e25e10cb3e2d68ea
vladyslav <kanendoc@gmail.com>
2023-06-22 20:28:53 +0300

    Покачто это первый пробник. Класс написанный под моторы. Методы предоставляют текущую информацию о состоянии движения моторов.

--- controller_module/motors.py
+++ controller_module/motors.py
@@ -1,4 +1,4 @@
-#import Jetson.GPIO as GPIO
+import Jetson.GPIO as GPIO
 import time
 class Motors: #Создаём класс моторы
     def __init__(self, stepPin, dirPin): # Для каждого мотора пин шаговый (ШИМ) и пин направления
@@ -13,29 +13,30 @@ class Motors: #Создаём класс моторы
         self.final_time = 0
         self.angular_velocity = 0
         self.time_to_rotate = 0
+
         # для меня
         #print(stepPin, ' ', dirPin)
 
-    def moveClockwise(self, steps):
-        #GPIO.output(self.dirPin, GPIO.HIGH)
+    def moveClockwise(self, angle_to_tilt):
+        GPIO.output(self.dirPin, GPIO.HIGH)
         self.begin_time = time.time()
-        for _ in range(steps):
-            #GPIO.output(self.stepPin, GPIO.HIGH)
-            #GPIO.output(self.stepPin, GPIO.LOW)
-            self.current_steps += 1
-            # для меня
-            print(self.stepPin, ' ', self.dirPin, ' ', steps)
-        self.final_time = time.time()
+        if angle_to_tilt < 90:
+            for _ in range(angle_to_tilt/0.225):# 0,225 это соотношение делителя на драйвере двигателя и 360гр
+                GPIO.output(self.stepPin, GPIO.HIGH)# получаем количество шагов на которые надо прокрутить
+                GPIO.output(self.stepPin, GPIO.LOW)
+                self.current_steps += 1
+                # для меня
+                #print(self.stepPin, ' ', self.dirPin, ' ', angle_to_tilt)
+            self.final_time = time.time()
 
-    def moveCntrClockwise(self, steps, ):
+    def moveCntrClockwise(self, angle_to_tilt):
         self.begin_time = time.time()
-        #GPIO.output(self.dirPin, GPIO.HIGH)
-        for _ in range(steps):
-            #GPIO.output(self.stepPin, GPIO.HIGH)
-            #GPIO.output(self.stepPin, GPIO.LOW)
-
+        GPIO.output(self.dirPin, GPIO.HIGH)
+        for _ in range(angle_to_tilt/0.225):
+            GPIO.output(self.stepPin, GPIO.HIGH)
+            GPIO.output(self.stepPin, GPIO.LOW)
             # для меня
-            print(self.stepPin, ' ', self.dirPin, ' ', steps)
+            #print(self.stepPin, ' ', self.dirPin, ' ', steps)
     def getAbsAngle(self, steps):
         self.abs_angle = steps / 1600
         return self.abs_angle
@@ -48,5 +49,5 @@ class Motors: #Создаём класс моторы
     def getAngularVelocity(self):
         return self.abs_angle/self.time_to_rotate
 
-# Создание экземпляра класса
-#xMoto = Motors.moveClockwise(42, 1000)
+
+
